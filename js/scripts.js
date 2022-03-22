8535dcbf3d6d6de66fa98232c2fa483095536f1f
NxBlaze <lotko.lukasz@gmail.com>
2022-03-22 19:19:42 +0100

    Refactor drawing grid as a separate function

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,10 @@ const canvas = document.getElementById('canvas');
 let drawing = false;
 const root = document.querySelector(':root');
 
-let gridSize = 100;
+let gridSize = 30;
 root.style.setProperty('--gridSize', gridSize);
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    let gridPixel = document.createElement('div');
-    gridPixel.classList.add('grid');
-    canvas.appendChild(gridPixel);
-  }
-}
+drawGrid(gridSize);
 
 canvas.addEventListener('mousedown', (e) => {
   drawing = true;
@@ -32,3 +26,13 @@ canvas.addEventListener('mousemove', (e) => {
   e.preventDefault;
   if (drawing) e.target.style.backgroundColor = '#000';
 });
+
+function drawGrid(size) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let gridPixel = document.createElement('div');
+      gridPixel.classList.add('grid');
+      canvas.appendChild(gridPixel);
+    }
+  }
+}
